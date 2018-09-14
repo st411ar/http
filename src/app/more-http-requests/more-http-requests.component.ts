b9b96401682951898ec09a04efd9de1c952fcdec
Vitaly Kiselev <st411ar@ya.ru>
2018-09-14 13:07:55 +0300

    created more-http-requests component

---
+++ src/app/more-http-requests/more-http-requests.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-more-http-requests',
+	templateUrl: './more-http-requests.component.html'
+})
+export class MoreHttpRequestsComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit() {}
+
+}
