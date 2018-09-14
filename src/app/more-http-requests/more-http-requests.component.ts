569c180fe0f5d2c51c655b3c329888207693f9c5
Vitaly Kiselev <st411ar@ya.ru>
2018-09-14 13:23:26 +0300

    created makePost method

--- src/app/more-http-requests/more-http-requests.component.ts
+++ src/app/more-http-requests/more-http-requests.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Response } from '@angular/http';
 
 @Component({
 	selector: 'app-more-http-requests',
 	templateUrl: './more-http-requests.component.html'
 })
 export class MoreHttpRequestsComponent implements OnInit {
+	isLoading: boolean;
+	data: any;
 
-	constructor() {}
+	constructor(private http: HttpClient) {}
 
 	ngOnInit() {}
+
+
+	makePost(): void {
+		this.isLoading = true;
+		this.http.post(
+			'https://jsonplaceholder.typicode.com/posts',
+			JSON.stringify({
+				body: 'bar',
+				title: 'foo',
+				userId: 1
+			})
+		).subscribe(
+			(data: Response) => {
+				this.data = data;
+				this.isLoading = false;
+			}
+		);
+	}
 
 }
